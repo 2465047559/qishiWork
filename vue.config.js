ef04329f66c4d0e9cb6ea3f6f51143814a15e019
阳奈奈 <2465047559@qq.com>
2020-12-19 13:21:45 +0800

    骑士作业后单后端
    Signed-off-by: 阳奈奈 <2465047559@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,26 @@
 module.exports = {
-  // 配置跨域代理
+  // 配置跨域代理  开发的时候
+  // devServer: {
+  //   // Paths
+  //   host: 'localhost',
+  //   port: '8081',
+  //   https: false,
+  //   open: true,
+  //   proxy: {
+  //     '/api': { // 匹配所有以 '/api'开头的请求路径
+  //       target: 'https://api.weixin.qq.com', // 代理目标的基础路径
+  //       changeOrigin: true, // 支持跨域
+  //       pathRewrite: {// 重写路径: 去掉路径中开头的'/api'
+  //         '^/api': ''
+  //       }
+  //     }
+  //   },
+  // }
+
   devServer: {
     // Paths
-    host: 'localhost',
-    port: '8081',
+    host: 'https://work-admin-9gkeh21qfd5d346e-1258768616.tcloudbaseapp.com',
+    port: '8080',
     https: false,
     open: true,
     proxy: {
@@ -16,4 +33,6 @@ module.exports = {
       }
     },
   }
+
+
 }
